70c91957a596d9f2b7b1cbd8f8ec26110a85aef7
Umar Farooq <umarisonline007@gmail.com>
2026-01-10 14:08:08 +0500

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,21 @@ import express from "express";
 const app = express();
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
+import path from "path";
+
+const __dirname = path.resolve();
+console.log(__dirname);
 
 app.use("/api/auth", authRoutes);
+
+// make ready for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*all", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
