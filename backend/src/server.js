36151ed97de781dac6e0dee51376a393c694a814
Umar Farooq <umarisonline007@gmail.com>
2026-07-18 11:27:39 +0500

    Implemened the signup page UI

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,14 @@ import express from "express";
 const app = express();
 import path from "path";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js"
+import messageRoutes from "./routes/message.route.js";
 import connectDB from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
