9f894b496b032f924b94195a2a9edea11e636e38
Umar Farooq <umarisonline007@gmail.com>
2026-07-19 12:26:22 +0500

    Implemented login page UI

--- frontend/src/components/BorderAnimatedContainer.jsx
+++ frontend/src/components/BorderAnimatedContainer.jsx
@@ -1,6 +1,6 @@
 const BorderAnimatedContainer = ({ children }) => {
   return (
-    <div className="w-full h-full [background:linear-gradient(45deg,#172033,theme(colors.slate.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.slate.600/.48)_80%,_theme(colors.cyan.500)_86%,_theme(colors.cyan.300)_90%,_theme(colors.cyan.500)_94%,_theme(colors.slate.600/.48))_border-box] rounded-2xl border border-transparent animate-border  flex overflow-hidden">
+    <div className="w-full h-full [background:linear-gradient(45deg,#172033,--theme(--color-slate-800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),--theme(--color-slate-600/.48)_80%,--theme(--color-cyan-500)_86%,--theme(--color-cyan-300)_90%,--theme(--color-cyan-500)_94%,--theme(--color-slate-600/.48))_border-box] rounded-2xl border border-transparent animate-border flex overflow-hidden">
       {children}
     </div>
   );
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -24,112 +24,112 @@ const SignUpPage = () => {
   };
 
   return (
-    <div className="w-full max-w-6xl mx-auto bg-slate-900">
-      <div className="relative w-full max-w-6xl h-auto max-h-[95vh]">
-        {/* <BorderAnimatedContainer> */}
-        <div className="w-full flex flex-col md:flex-row">
-          {/* Form Column - Left Side */}
-          <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
-            <div className="w-full max-w-md">
-              {/* Heading Text */}
-              <div className="text-center mb-8">
-                <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
-                <h2 className="text-2xl font-bold text-slate-200 mb-2">
-                  Create Account
-                </h2>
-                <p className="text-slate-400">Sign up for a new account</p>
-              </div>
-              {/* Form */}
-              <form onSubmit={handleSubmit} className="space-y-6">
-                {/* Full-Name Input */}
-                <div>
-                  <label className="auth-input-label">Full Name</label>
-                  <div className="relative">
-                    <UserIcon className="auth-input-icon" />
-                    <input
-                      type="text"
-                      value={formData.fullName}
-                      onChange={(e) =>
-                        setFormData({ ...formData, fullName: e.target.value })
-                      }
-                      className="input"
-                      placeholder="John Doe"
-                    />
-                  </div>
+    <div className="absolute w-full max-w-6xl mx-auto bg-slate-900 rounded-2xl">
+      <div className="w-full max-w-6xl h-auto max-h-[95vh]">
+        <BorderAnimatedContainer>
+          <div className="w-full flex flex-col md:flex-row">
+            {/* Form Column - Left Side */}
+            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
+              <div className="w-full max-w-md">
+                {/* Heading Text */}
+                <div className="text-center mb-8">
+                  <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
+                  <h2 className="text-2xl font-bold text-slate-200 mb-2">
+                    Create Account
+                  </h2>
+                  <p className="text-slate-400">Sign up for a new account</p>
                 </div>
-                {/* Email Input*/}
-                <div>
-                  <label className="auth-input-label">Email</label>
-                  <div className="relative">
-                    <UserIcon className="auth-input-icon" />
-                    <input
-                      type="email"
-                      value={formData.email}
-                      onChange={(e) =>
-                        setFormData({ ...formData, email: e.target.value })
-                      }
-                      className="input"
-                      placeholder="abc@gmail.com"
-                    />
+                {/* Form */}
+                <form onSubmit={handleSubmit} className="space-y-6">
+                  {/* Full-Name Input */}
+                  <div>
+                    <label className="auth-input-label">Full Name</label>
+                    <div className="relative">
+                      <UserIcon className="auth-input-icon" />
+                      <input
+                        type="text"
+                        value={formData.fullName}
+                        onChange={(e) =>
+                          setFormData({ ...formData, fullName: e.target.value })
+                        }
+                        className="input"
+                        placeholder="John Doe"
+                      />
+                    </div>
                   </div>
-                </div>
-                {/* Password Input */}
-                <div>
-                  <label className="auth-input-label">Password</label>
-                  <div className="relative">
-                    <UserIcon className="auth-input-icon" />
-                    <input
-                      type="password"
-                      value={formData.password}
-                      onChange={(e) =>
-                        setFormData({ ...formData, password: e.target.value })
-                      }
-                      className="input"
-                      placeholder="Enter your password"
-                    />
+                  {/* Email Input*/}
+                  <div>
+                    <label className="auth-input-label">Email</label>
+                    <div className="relative">
+                      <MailIcon className="auth-input-icon" />
+                      <input
+                        type="email"
+                        value={formData.email}
+                        onChange={(e) =>
+                          setFormData({ ...formData, email: e.target.value })
+                        }
+                        className="input"
+                        placeholder="abc@gmail.com"
+                      />
+                    </div>
                   </div>
-                </div>
+                  {/* Password Input */}
+                  <div>
+                    <label className="auth-input-label">Password</label>
+                    <div className="relative">
+                      <LockIcon className="auth-input-icon" />
+                      <input
+                        type="password"
+                        value={formData.password}
+                        onChange={(e) =>
+                          setFormData({ ...formData, password: e.target.value })
+                        }
+                        className="input"
+                        placeholder="Enter your password"
+                      />
+                    </div>
+                  </div>
 
-                {/* Signup Button */}
-                <button className="auth-btn" disabled={isSigningUp}>
-                  {isSigningUp ? (
-                    <LoaderIcon className="w-full h-5 animate-spin text-center" />
-                  ) : (
-                    "Create account"
-                  )}
-                </button>
-              </form>
+                  {/* Signup Button */}
+                  <button className="auth-btn" disabled={isSigningUp}>
+                    {isSigningUp ? (
+                      <LoaderIcon className="w-full h-5 animate-spin text-center" />
+                    ) : (
+                      "Create account"
+                    )}
+                  </button>
+                </form>
 
-              <div className="mt-6 text-center">
-                <Link to="/login" className="auth-link">
-                  Already have an account? Login
-                </Link>
-              </div>
-            </div>
-          </div>
-          {/* Form Illustration - Right Side */}
-          <div className="hidden md:w-1/2 md:flex items-center justify-center p-6 bg-linear-to-bl from slate-800/20 to-transparent ">
-            <div>
-              <img
-                src="/signup.png"
-                alt="People using mobile devices"
-                className="w-full h-auto object-contain"
-              />
-              <div className="mt-6 text-center">
-                <h3 className="text-xl font-medium text-cyan-400">
-                  Start Your Journey Today
-                </h3>
-
-                <div className="mt-4 flex justify-center gap-4">
-                  <span className="auth-badge">Free</span>
-                  <span className="auth-badge">Easy Setup</span>
-                  <span className="auth-badge">Private</span>
+                <div className="mt-6 text-center">
+                  <Link to="/login" className="auth-link">
+                    Already have an account? Login
+                  </Link>
                 </div>
               </div>
             </div>
+            {/* Form Illustration - Right Side */}
+            <div className="hidden md:w-1/2 md:flex items-center justify-center p-6 bg-linear-to-bl from slate-800/20 to-transparent ">
+              <div>
+                <img
+                  src="/signup.png"
+                  alt="People using mobile devices"
+                  className="w-full h-auto object-contain"
+                />
+                <div className="mt-6 text-center">
+                  <h3 className="text-xl font-medium text-cyan-400">
+                    Start Your Journey Today
+                  </h3>
+
+                  <div className="mt-4 flex justify-center gap-4">
+                    <span className="auth-badge">Free</span>
+                    <span className="auth-badge">Easy Setup</span>
+                    <span className="auth-badge">Private</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-        {/* </BorderAnimatedContainer> */}
+        </BorderAnimatedContainer>
       </div>
     </div>
   );
